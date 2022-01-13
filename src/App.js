41b640daddb22adff5bf919a59723a0d2ed7a0b5
JohnPaulsj <97438834+JohnPaulsj@users.noreply.github.com>
2022-01-13 15:35:56 +0530

    commit all stages of the day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import img1 from './img1.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      John Paul First Project
-      </header>
-    </div>
+ 
+ <div >
+   <div>
+     <h1>
+     hi jesuits... how r u all 
+     </h1>
+     <img src={img1} />
+   </div>
+    
+<div style={{textAlign:'center'}}>
+  <h2>
+  i am a fresher.... 
+  </h2>
+  <img src={img1} />
+  </div>
+
+<div style={{textAlign:'right', fontSize:'120px', display: 'none'}}>
+    <h3>
+
+  so bear with me...
+ </h3>
+ < img src={img1} />
+
+ </div>
+  </div>
+
   );
 }
 
